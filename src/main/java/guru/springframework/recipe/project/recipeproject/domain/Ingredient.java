daff5bf84ee64fec80301cc8954ffc1eb07aef0d
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-10-31 22:45:24 +0000

    changed to gradle project. removed jpa stuff

--- src/main/java/guru/springframework/recipe/project/recipeproject/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipe/project/recipeproject/domain/Ingredient.java
@@ -3,26 +3,19 @@ package guru.springframework.recipe.project.recipeproject.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 //avoid addind recipe in hashcode as can cause circular reference error
 @EqualsAndHashCode(exclude = {"recipe"})
-@Entity
 public class Ingredient {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private BigDecimal amount;
 
-    //LOAD UOM WHEN SOMEONE LOADS Ingredient
-    @OneToOne (fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
 
-    @ManyToOne
     private Recipe recipe;
 
     //have to add empty one otherwise hibernate will throw error
--- src/main/java/guru/springframework/recipe/project/recipeproject/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe/project/recipeproject/domain/Recipe.java
@@ -3,17 +3,15 @@ package guru.springframework.recipe.project.recipeproject.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
-@Entity
+
 public class Recipe {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String description;
@@ -22,28 +20,21 @@ public class Recipe {
     private Integer servings;
     private Integer source;
     private String url;
-    @Lob
+
     private String directions;
 
     //cascase all persist all operations, Recipe is owner. Will be stored in ingredient recipe attribute
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients = new HashSet<>();
 
-    @Lob //for large information in hibernate as hibernate only support 255 characted max. Will be stored is clob in hibernate
-    private Byte[] image;
+     private Byte[] image;
 
     //Use string rather than ordinal as ordinal takes index of enum and if number changes the index will be messed up
-    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
-    @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     //added join table as only need one table to show recipes to categories. Without it two tables would be created
-    @ManyToMany
-    @JoinTable(name = "recipe_category",
-            joinColumns = @JoinColumn(name = "recipe_id"),
-    inverseJoinColumns = @JoinColumn(name = "category_id"))
+
     private Set<Category> categories = new HashSet<>();
 
     //since method already here lombok wont override it
