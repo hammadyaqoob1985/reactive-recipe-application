40acd899562b2ec7bb6647cdc56b7279dfd3c959
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-12-25 17:23:55 +0000

    added router test functions

---
+++ src/test/java/guru/springframework/recipe/project/recipeproject/controllers/RouterFunctionTest.java
@@ -0,0 +1,63 @@
+package guru.springframework.recipe.project.recipeproject.controllers;
+
+
+import guru.springframework.recipe.project.recipeproject.config.WebConfig;
+import guru.springframework.recipe.project.recipeproject.domain.Recipe;
+import guru.springframework.recipe.project.recipeproject.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Flux;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by jt on 8/29/17.
+ */
+
+public class RouterFunctionTest {
+
+    WebTestClient webTestClient;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        WebConfig webConfig = new WebConfig();
+
+        RouterFunction<?> routerFunction = webConfig.routes(recipeService);
+
+        webTestClient = WebTestClient.bindToRouterFunction(routerFunction).build();
+    }
+
+
+    @Test
+    public void testGetRecipes() throws Exception {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just());
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    public void testGetRecipesWithData() throws Exception {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just(new Recipe(), new Recipe()));
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Recipe.class);
+    }
+}
