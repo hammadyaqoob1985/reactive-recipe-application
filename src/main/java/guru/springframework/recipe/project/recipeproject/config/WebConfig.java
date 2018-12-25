b2046135256834fecce20ad69a8e6380905fccd7
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-12-25 17:13:50 +0000

    made initial api which returns all recipes but only works with rective libraries

---
+++ src/main/java/guru/springframework/recipe/project/recipeproject/config/WebConfig.java
@@ -0,0 +1,24 @@
+package guru.springframework.recipe.project.recipeproject.config;
+
+import guru.springframework.recipe.project.recipeproject.domain.Recipe;
+import guru.springframework.recipe.project.recipeproject.services.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route(GET("/api/recipes"),
+                serverRequest -> ServerResponse.ok()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(recipeService.getRecipes(), Recipe.class));
+    }
+}
