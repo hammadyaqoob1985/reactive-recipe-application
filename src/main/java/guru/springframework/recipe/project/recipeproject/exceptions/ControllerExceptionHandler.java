e8f1fef7790f18b50bd63d824065bd3f5f1b7f1f
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-12-25 16:59:07 +0000

    added in global advice for handling global errors

--- src/main/java/guru/springframework/recipe/project/recipeproject/exceptions/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/recipe/project/recipeproject/exceptions/ControllerExceptionHandler.java
@@ -2,28 +2,27 @@ package guru.springframework.recipe.project.recipeproject.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 //import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    //Add response status here other 200 will be returned not 404
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormatException(Exception exception) {
-//        log.error("Handling Number Format exceptionError");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("400error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//
-//    }
+    //Add response status here other 200 will be returned not 404
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormatException(Exception exception, Model model) {
+        log.error("Handling binding exceptionError");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+
+        return "400error";
+
+    }
 }
