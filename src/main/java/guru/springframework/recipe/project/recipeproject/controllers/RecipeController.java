8adae565ff6405c8b314797b803c152b1dc39aa5
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-12-25 11:06:03 +0000

    @valid does not work with webflux. Use webdatabinder which is way to validate without annotations

--- src/main/java/guru/springframework/recipe/project/recipeproject/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipe/project/recipeproject/controllers/RecipeController.java
@@ -4,10 +4,12 @@ import guru.springframework.recipe.project.recipeproject.commands.RecipeCommand;
 import guru.springframework.recipe.project.recipeproject.exceptions.NotFoundException;
 import guru.springframework.recipe.project.recipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.servlet.ModelAndView;
 
@@ -19,6 +21,15 @@ public class RecipeController {
 
     public static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     RecipeService recipeService;
+
+
+    private WebDataBinder webDataBinder;
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+
+        this.webDataBinder = webDataBinder;
+    }
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -51,7 +62,12 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand, BindingResult bindingResult) {
+    public String saveOrUpdate(@ModelAttribute("recipe") RecipeCommand recipeCommand) {
+
+        webDataBinder.validate();
+
+        BindingResult bindingResult = webDataBinder.getBindingResult();
+
 
         if(bindingResult.hasErrors()) {
             bindingResult.getAllErrors().forEach(error -> {
